ef08e1279c7d1ea2caf4d94c10b5d2f67412b1da
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-19 16:32:57 +0530

    variable

---
+++ variablesconcept.js
@@ -0,0 +1,9 @@
+const accountID = 24555;
+let accountEmail = "swapnbcdb@nbhbdc.com";
+var accountName ="swapdil";
+ acountState="maharashtra";
+ let name;
+
+ accountEmail =" swapnil@gmail.com";
+
+ console.table([accountID,accountEmail,accountName,acountState,name]);

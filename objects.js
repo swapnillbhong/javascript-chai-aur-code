a6b3f62e9eeba990a54289bb629ab43bc9ad0dbd
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-20 17:42:11 +0530

    object access

---
+++ objects.js
@@ -0,0 +1,10 @@
+const myObj={
+    "name":"swapnil",
+    "fullname":"swapnilbhong",
+    Age:23,
+    email:"swapnil@google.com",
+    isLoggIn:false,
+    lostWorkingdays:["satruday","Sunday"]
+}
+
+console.log(myObj.fullname);

7a759a5421672815f531ed00c3ea7c188fc02fc1
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-20 17:14:21 +0530

    array function

---
+++ arrayinjavascript01.js
@@ -0,0 +1,5 @@
+const marvel_heros=["spiderman","ironman","thor"]
+const dc_heros=["powerranger","batman","superman"]
+
+const all_heros=marvel_heros.concat(dc_heros);//to merge two arrays and get a new array
+console.log(all_heros);

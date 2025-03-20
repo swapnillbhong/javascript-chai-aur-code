7b185bd25ba17da4f0cd0e3c78a669b930b94ba3
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-20 12:41:06 +0530

    string interpolation

---
+++ string.js
@@ -0,0 +1,5 @@
+let name = "swapnil"
+let repoCount = 50
+
+// console.log(name + repoCount + "value")
+console.log(`hello my name is ${name} and mmy repo count is ${repoCount} `)//mordern type of writing 

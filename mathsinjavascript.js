2968d0a952c1fd32041f50d31e179c9de4c6c593
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-20 15:48:53 +0530

    math concept

---
+++ mathsinjavascript.js
@@ -0,0 +1,7 @@
+console.log (Math.abs(-4))//for converting negative to positive 
+console.log(Math.round(4.64))//for round off the value
+console.log(Math.ceil(4.1))//to convert upper numbre
+console.log(Math.floor(4.35))// to get lower number value
+console.log(Math.min(2,4,2,1,5,))//to get the minimum value from the array
+console.log(Math.max(2,5,4,8,4,445))//to get the maximum value from the array
+console.log(Math.random())

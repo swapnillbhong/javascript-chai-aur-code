235682c1bf8fe00b6c067928ba0940d16a5bed64
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-20 16:48:32 +0530

    comment

--- arrayinjavascipt.js
+++ arrayinjavascipt.js
@@ -1,5 +1,5 @@
 const myArr=[2,3,5,4,7,8]//normal array declaration 
-console.log(myArr)
+//console.log(myArr)
 
 const arrOne=new Array(2,5,45,7)//array as a object
-console.log(arrOne)
+//console.log(arrOne)

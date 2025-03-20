33187d2c4e1b370843aa579390a3c284739e27ab
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-20 17:16:59 +0530

    split function

--- arrayinjavascript01.js
+++ arrayinjavascript01.js
@@ -2,4 +2,7 @@ const marvel_heros=["spiderman","ironman","thor"]
 const dc_heros=["powerranger","batman","superman"]
 
 const all_heros=marvel_heros.concat(dc_heros);//to merge two arrays and get a new array
-console.log(all_heros);
+//console.log(all_heros);
+
+const all_new_heros=[...marvel_heros,...dc_heros]//split function in array
+console.log(all_new_heros)

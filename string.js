895e65de57c462616493e047a25d2065f4f1c7d0
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-20 12:52:52 +0530

    basic functions of strings

--- string.js
+++ string.js
@@ -3,3 +3,11 @@ let repoCount = 50
 
 // console.log(name + repoCount + "value")
 console.log(`hello my name is ${name} and mmy repo count is ${repoCount} `)//mordern type of writing 
+
+const gameName = new String('GodFather')//declare string as a object
+
+console.log(typeof  gameName);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('a')) 

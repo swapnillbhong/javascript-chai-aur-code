ba405c595943ec1b8c4db6bfde75bec8cc22ae0c
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-20 16:30:09 +0530

    date and time

--- datesection.js
+++ datesection.js
@@ -1,6 +1,11 @@
-let myDate= new Date()
-console.log(myDate.toString())
-console.log(myDate.toDateString())
-console.log(myDate.toISOString())
-console.log(myDate.toJSON())
-console.log(myDate.toLocaleDateString())
+// let myDate= new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+
+
+// let createDate = new Date(2024,0,23)
+let createDate = new Date(2024,0,23,5,3)
+console.log(createDate.toLocaleString());

efe3c238c69d20b3dd36a3bcdb221883c9746278
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-20 13:47:28 +0530

    string functions

--- string.js
+++ string.js
@@ -11,3 +11,16 @@ console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2))
 console.log(gameName.indexOf('a')) 
+
+const newStringgame = gameName.substring(0,4);
+console.log(newStringgame);
+
+const strigcut = gameName.slice(-8,4)
+
+console.log(strigcut)
+
+
+const stingone= "     hello.     "
+
+console.log(stingone)
+console.log(stingone.trim())

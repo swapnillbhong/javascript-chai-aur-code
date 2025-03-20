0346dcaffe38dd7c63f2eb25d32990ede2a8a9ab
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-20 17:26:14 +0530

    array concept

--- arrayinjavascript01.js
+++ arrayinjavascript01.js
@@ -6,3 +6,12 @@ const all_heros=marvel_heros.concat(dc_heros);//to merge two arrays and get a ne
 
 const all_new_heros=[...marvel_heros,...dc_heros]//split function in array
 console.log(all_new_heros)
+
+console.log(Array.isArray("swapnil"));//to check it is array or not
+console.log(Array.from("swapnil")); // to convert into array
+
+let scoore1 =100
+let scoore2 =100
+let scoore3 =100
+
+console.log(Array.of(scoore1,scoore2,scoore3)) //to convert mutliple values into array

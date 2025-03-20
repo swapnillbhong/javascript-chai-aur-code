55def26043432ee2bc639545424fbaee1d26c99e
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-20 16:59:46 +0530

    array function

--- arrayinjavascipt.js
+++ arrayinjavascipt.js
@@ -3,3 +3,11 @@ const myArr=[2,3,5,4,7,8]//normal array declaration
 
 const arrOne=new Array(2,5,45,7)//array as a object
 //console.log(arrOne)
+
+// myArr.push(25)  //to add value 
+// myArr.pop()      // to remove value 
+
+myArr.unshift(235)// to add value in the front 
+myArr.shift() // to remove the value from the front
+console.log(myArr.includes(120));//to search the value from string and array 
+console.log(myArr)

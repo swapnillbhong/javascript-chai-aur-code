b097ed9ac3a98c5355d11e4e504a811a25768246
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-20 12:10:01 +0530

    symbol datatype

--- dataconverstion.js
+++ dataconverstion.js
@@ -1,8 +1,16 @@
 let score="33"
 
-console.log(typeof score)
+//console.log(typeof score)
 
 let ValueInNumber =Number(score)// conversion of one data type to another typecasting concept 
-console.log(typeof ValueInNumber)
+//console.log(typeof ValueInNumber)
 
-console.log(ValueInNumber)
+//console.log(ValueInNumber)
+
+
+let id=Symbol("1234")
+
+let anotherId=Symbol("1234")
+
+console.log(id===anotherId)
+

105b43e630f32d79f7a29e84639f8dc38facc8c5
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-20 16:35:41 +0530

    date

--- datesection.js
+++ datesection.js
@@ -3,9 +3,16 @@
 // console.log(myDate.toDateString())
 // console.log(myDate.toISOString())
 // console.log(myDate.toJSON())
-// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleDateString())//get current date and time 
 
 
 // let createDate = new Date(2024,0,23)
 let createDate = new Date(2024,0,23,5,3)
-console.log(createDate.toLocaleString());
+//console.log(createDate.toLocaleString());// to get the time and date accounting to need 
+
+
+//to get the time stamp for quiz and other options
+
+let myTimeStamp =Date.now()
+
+console.log(myTimeStamp);

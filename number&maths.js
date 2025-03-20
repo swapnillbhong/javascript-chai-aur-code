10d9bf9f76a337580c71fd1046684702a88ba2eb
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-20 13:56:04 +0530

    number and their functions

---
+++ number&maths.js
@@ -0,0 +1,10 @@
+const scoore = 100
+console.log(scoore)
+
+const balance = new Number(400.515141515)
+
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(typeof balance)
+console.log(balance.toFixed(2))

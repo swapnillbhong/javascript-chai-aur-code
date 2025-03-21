1570cf425b5a01a80f33fcc94eab3c47c03d11a5
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-21 11:58:21 +0530

    object access functions

--- objectconstructor.js
+++ objectconstructor.js
@@ -23,6 +23,36 @@ const userInsta={
 const obj1= { 1 :"a" ,2: "b"}
 const obj2= { 3 :"c" ,4: "d"}
 
-const obj3 =Object.assign(obj1,obj2)
-console.log(obj3)
+//const obj3 =Object.assign(obj1,obj2) // we can use the function like this 
+
+const obj3={...obj1,...obj2}//by using split method
+//console.log(obj3)
+
+// const user = [
+//     {
+//         id:1,
+//         email:"s@gmail.com"
+//     }
+//     {
+//         id:2,
+//         email:"s@gmail.com"
+//     }
+//     {
+//         id:3,
+//         email:"s@gmail.com"
+//     }
+//     {
+//         id:4,
+//         email:"s@gmail.com"
+//     }
+//     {
+//         id:5,
+//         email:"s@gmail.com"
+//     }
+// ]
+//user[1].id // to access the object from array 
+
+console.log(Object.keys(insta));// to access the object element as a array
+console.log(Object.values(insta));
+
 
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,22 @@
+onst user = [
+//     {
+//         id:1,
+//         email:"s@gmail.com"
+//     }
+//     {
+//         id:2,
+//         email:"s@gmail.com"
+//     }
+//     {
+//         id:3,
+//         email:"s@gmail.com"
+//     }
+//     {
+//         id:4,
+//         email:"s@gmail.com"
+//     }
+//     {
+//         id:5,
+//         email:"s@gmail.com"
+//     }
+// ]

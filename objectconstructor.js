41af37626eae1209c41327804ea764de8397008a
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-21 11:40:38 +0530

    adding one or more object value in a single object

--- objectconstructor.js
+++ objectconstructor.js
@@ -20,4 +20,9 @@ const userInsta={
 //nasted object can be accessable in javascript
 //console.log(userInsta.fullname.username.firstname)
 
+const obj1= { 1 :"a" ,2: "b"}
+const obj2= { 3 :"c" ,4: "d"}
+
+const obj3 =Object.assign(obj1,obj2)
+console.log(obj3)
 

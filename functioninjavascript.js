7d58698adbe94ffc1974d8e9e02de354a46b0e80
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-21 13:47:08 +0530

    functions

--- functioninjavascript.js
+++ functioninjavascript.js
@@ -11,5 +11,19 @@ function addition(num1,num2){
  //   console.log(num1+num2);
     return num1+num2
 }
-const result= addition(3,5)
-console.log("result:",result);
+const result= addition(3,5) 
+//console.log("result:",result);
+
+
+function LoginUserMessage(user){
+    return `${user} just log in `
+}
+
+//console.log(LoginUserMessage("swapnil"));
+
+
+function cart(...num4)//rest operator (...)for adding more than one value in same element
+{
+    return num4
+}
+console.log(cart(200,4000,50000));

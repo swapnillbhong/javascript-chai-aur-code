2918e04d663b71f5c533311390e660abf2934312
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-21 12:42:12 +0530

    BASIC SYNTAX

---
+++ functioninjavascript.js
@@ -0,0 +1,8 @@
+function myname(){ //BASIC SYNTAX
+    console.log("S");
+    console.log("W");
+    console.log("A");
+    console.log("P");
+    console.log("Y");
+}
+ myname()//FOR CALLING FUNCTION

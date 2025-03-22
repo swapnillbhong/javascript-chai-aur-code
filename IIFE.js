c2e2da11db95c7610d220f66af18eafe3cea0395
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-22 11:25:59 +0530

    end IIFA function syntax

--- IIFE.js
+++ IIFE.js
@@ -2,4 +2,8 @@
 (function dbconnection(){
     console.log(`db connected`)
     
+})();//to end the IIFE we have to mention semi-colon on the end 
+
+(function aurcode(){
+    console.log(`db connected two`);
 })()

25cf408236629ade486241fce9a4633b06ea5f65
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-24 12:15:24 +0530

    while loop

---
+++ whiledowhile.js
@@ -0,0 +1,16 @@
+
+let i=0
+while (i<=10) {
+   // console.log(i);
+    i = i+2
+    
+}
+
+myarr = ['flash','superman','saktiman'];
+let arr=0
+while(arr < myarr.length) {
+    console.log(myarr[arr]);
+    arr++
+    
+    
+}

e7336bcfd1bc79c1621dbbfd8249f23f6d93a978
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-24 13:52:43 +0530

    filter in loop

--- foreachloop.js
+++ foreachloop.js
@@ -31,6 +31,15 @@ const mycoding=[
     }
 ]
 mycoding.forEach( (items) =>{
-        console.log(items.languageFileName);
+     //   console.log(items.languageFileName);
         
 } )
+
+
+//filters 
+
+const myNum =[1,2,3,4,5,6,7,8,9,10]
+
+const newNum = myNum.filter( (num=> num > 4) )
+console.log(newNum);
+

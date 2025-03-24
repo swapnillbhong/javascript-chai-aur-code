2a2dbddbc192627b15d8b748c7b19533526581b1
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-24 13:00:57 +0530

    getting key and value from object

---
+++ forin.js
@@ -0,0 +1,11 @@
+// for in loop 
+const myobj ={
+    js:'javascipt',
+    java:'java',
+    cpp:'c++',
+}
+//for getting key and value
+for (const key in myobj) {
+    console.log(`${key}is for ${myobj[key]}`);
+    
+    }

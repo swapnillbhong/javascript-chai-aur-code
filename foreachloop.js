5d4cc49fbbc5d3da64cc3306a5fc3d7cc5db79de
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-24 13:30:49 +0530

    using arrow function

--- foreachloop.js
+++ foreachloop.js
@@ -1,6 +1,10 @@
 const coding=["js","py","cpp","java"]
 
 coding.forEach(function (items){
-    console.log(items);
+    //console.log(items);
     
 })
+//using arrow function 
+coding.forEach( (items)=>{
+    console.log(items)
+} )

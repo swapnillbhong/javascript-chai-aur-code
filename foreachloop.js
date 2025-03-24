b3f1e0dd9a07eb6f9f1dba3239155f8abc84f694
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-24 13:54:59 +0530

    we should use keyword while we are using scoope

--- foreachloop.js
+++ foreachloop.js
@@ -40,6 +40,8 @@ mycoding.forEach( (items) =>{
 
 const myNum =[1,2,3,4,5,6,7,8,9,10]
 
-const newNum = myNum.filter( (num=> num > 4) )
+const newNum = myNum.filter( num=> {
+    return num > 4
+} )
 console.log(newNum);
 

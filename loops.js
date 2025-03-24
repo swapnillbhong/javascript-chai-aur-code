ba262c5beff048b46edf28e67af280ef0fae39d8
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-24 12:04:47 +0530

    for loop break and continue

---
+++ loops.js
@@ -0,0 +1,27 @@
+//for 
+
+for (let index = 0; index <=10; index++) {
+    const element = index;
+  //  console.log(element);
+    
+    
+}
+
+//break 
+
+for (let i = 0; i < 20; i++) {
+    if(i==5)
+    {
+        console.log(`detected 5`);
+        break//using break 
+    }
+   // console.log(i)
+}
+for (let i = 0; i < 20; i++) {
+    if(i==5)
+    {
+        console.log(`detected 5`);
+        continue 
+    }
+   console.log(i)
+}

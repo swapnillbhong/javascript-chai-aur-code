a996f542db646141dd18b24b6e6883bab1a60b94
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-26 13:54:08 +0530

    complete project

--- DOM/project/chaiaurcode.js
+++ DOM/project/chaiaurcode.js
@@ -6,15 +6,29 @@ const ramdomColor = function () {
     for (let i = 0; i < 6; i++) {
       color += hex[Math.floor(Math.random() * 16)];
     }
-    return color
+    return color;
   };
-
+  let intervalId
 const startChangingColor = function(){
-  document.body.style.backgroundColor = ramdomColor();
+
+  if(!intervalId)
+  {
+    intervalId= setInterval(chnagebgColour,1000)
+  }
+  function chnagebgColour(){
+     
+    document.body.style
+    .backgroundColor = ramdomColor();
+  }
 };
   
 
-const stopChangingColor = function(){};
+const stopChangingColor = function(){
+
+  clearInterval(intervalId);
+  intervalId = null;
+  
+};
 
 document.getElementById('start')
   .addEventListener('click',startChangingColor);

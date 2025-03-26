3a4eb847bcabc95b0aef34d8e5dd75ff363bcb5e
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-26 17:20:59 +0530

    then catch function in promise

--- promises.js
+++ promises.js
@@ -34,4 +34,31 @@ promiseThree.then(function(user){
     console.log(user)
 })
 
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false;
+          if(!error)
+            {
+                resolve({
+                     username:"hitesh",
+                     password :122
+                });
+            } 
+            else
+            {
+                 reject('Error : something went very wrong')
+            } 
+    },1000)
+})
 
+promiseFour
+.then((user)=>{
+    console.log(user)
+     return user.username
+})
+.then((username)=>{
+        console.log(username);
+})
+.catch(function(error){
+    console.log(error)
+})

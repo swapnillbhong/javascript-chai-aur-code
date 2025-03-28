38042fcdcda915c38608f2be2b4e56f0aa9f280c
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-28 16:20:51 +0530

    promises

--- promises.js
+++ promises.js
@@ -108,6 +108,6 @@ fetch('https://jsonplaceholder.typicode.com/users')
 .then( (response)=>{
     return response.json()
 }).then( (data)=>{
-    console.log(data)
+    console.log(data    )
 })
 .catch( (error)=> console.log("error"))

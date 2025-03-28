4165e858c8a542bd40da8bdacc67797c8f65bb47
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-28 19:02:30 +0530

    remove error

--- CURD_operations_JS/script.js
+++ CURD_operations_JS/script.js
@@ -1,22 +1,22 @@
-let selectedRow =null;
+let selectedRow = null;
 
 // show alert
 
-function showAlert(message, className){
-    const div=document.createElement("div");
+function showAlert(message, className) {
+    const div = document.createElement("div");
     div.className = `alert alert-${className}`;
 
 
     div.appendChild(document.createTextNode(message));
     const container = document.querySelector(".container")
     const main = document.querySelector(".main");
-    container.insertBefore(div,main);
+    container.insertBefore(div, main);
 
-    setTimeout( () => document.querySelector(".alert").remove(),3000);
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
 }
 
 // clear all fields 
-function clearFields(){
+function clearFields() {
     document.querySelector("#firstName").value = "";
     document.querySelector("#lastName").value = "";
     document.querySelector("#rollNo").value = "";
@@ -24,24 +24,24 @@ function clearFields(){
 
 // Add data
 
-document.querySelector("#student-form").addEventListener("submit", (e)=>{
+document.querySelector("#student-form").addEventListener("submit", (e) => {
     e.preventDefault();
 
     //get form values 
-    const firstName = document.querySelector("#firstName").value;
-    const lastName = document.querySelector("#lastName").value;
+    const firstName = document.querySelector("#Firstname").value;
+    const lastName = document.querySelector("#LastName").value;
     const rollNo = document.querySelector("#rollNo").value;
 
     // validate
-    if(firstName == "" || lastName == "" || rollNo == ""){
-        showAlert("please fill the form first","danger");
+    if (firstName == "" || lastName == "" || rollNo == "") {
+        showAlert("please fill the form first", "danger");
     }
-    else{
-        if(selectedRow == null){
+    else {
+        if (selectedRow == null) {
             const list = document.querySelector("#student-list");
             const row = document.createElement("tr");
-
-            row.innerHTML= `
+            console.log(firstName);
+            row.innerHTML = `
              <td>${firstName}</td>
              <td>${lastName}</td>
              <td>${rollNo}</td>
@@ -51,15 +51,15 @@ document.querySelector("#student-form").addEventListener("submit", (e)=>{
              </td>
             `;
             list.appendChild(row);
-            selectedRow = null; 
-            showAlert("student Added","success");
+            selectedRow = null;
+            showAlert("student Added", "success");
         }
-        else{
+        else {
             selectedRow.children[0].textContent = firstName;
             selectedRow.children[1].textContent = lastNameName;
             selectedRow.children[2].textContent = rollNo;
-            selectedRow = null ;
-            showAlert("Student info edited","info");
+            selectedRow = null;
+            showAlert("Student info edited", "info");
         }
         clearFields();
     }
@@ -67,10 +67,10 @@ document.querySelector("#student-form").addEventListener("submit", (e)=>{
 
 // delete data 
 
-document.querySelector("#student-list").addEventListener("click",(e) =>{
+document.querySelector("#student-list").addEventListener("click", (e) => {
     target = e.target;
-    if(target.classList.contains("delete")){
+    if (target.classList.contains("delete")) {
         target.parentElement.parentElement.remove();
-        showAlert("Student Data Deleted ","danger")
+        showAlert("Student Data Deleted ", "danger")
     }
 })

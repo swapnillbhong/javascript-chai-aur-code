ac177abee6276663c595250513d6b1afceddf601
swapnillbhong <swapnilbhong1627@gmail.com>
2025-03-28 16:35:28 +0530

    getter setter property

---
+++ get&setprop.js
@@ -0,0 +1,24 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}hitesh`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const propName = new User("Swapnil@i.ai", "abc")
+console.log(propName.email);
